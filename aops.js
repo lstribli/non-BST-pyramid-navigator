bc3113b6bfbfa5f8d0d99e71c5a61724cc68399d
Logan Stribling <logan.stribling@gmail.com>
2020-10-10 00:56:31 -0600

    added more ideas

--- aops.js
+++ aops.js
@@ -2,25 +2,49 @@ console.time('time');
 let Target = 720;
 
 let input = [2, 4, 3, 3, 2, 6, 2, 9, 5, 2, 10, 5, 2, 15, 5];
-let size = input.length;
-let ind = input.length - 1;
-let base = ind - 5;
-let fl = base - 4;
-let tl = fl - 3;
-let sl = tl - 2;
-let FL = sl - 1;
 
-const factor = (Target, input) => {
 
-};
-
-for (let i = size; i > 0; i--) {
-  while (i !== input[base]) {
-
+const nav = (Target, input) => {
+  if (input !== []) {
+    console.log('invalid input type');
+  }
+  if (input.length % 2 === 0) {
+    console.log('pyramid uneven');
+  }
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== isNaN()) {
+      console.log('an element in input is NaN')
+    }
   }
 }
 
 
+
+
+
+
+const nav = (Target, input) => {
+  let product = null;
+  let top = null;
+  let levelCounter = 0;
+
+
+  for (let i = 0; i < input.length; i++) {
+    if (i === input[0]) {
+      top = i;
+      product = i;
+      levelCounter++;
+    }
+    if () {
+
+      (levelCounter * 1) + 1;
+    }
+    if () {
+
+    }
+  }
+
+}
 
 
 
@@ -34,60 +58,90 @@ for (let i = size; i > 0; i--) {
 
 //   if (input[0] !== null) {
 //     current = input[0];
-//     Object.assign(algoObj, { 'level 1': current });
+//     Object.assign(algoObj, { 'one': current });
 //     current = null;
 //     counter++;
-//     console.log(algoObj);
 //   }
 
 //   if (counter === 1) {
-//     let tempArr = [];
+//     let tempObj = {};
 
-//     tempArr.push(input[1], input[2]);
-//     current = tempArr;
-//     Object.assign(algoObj, { 'level 2': current });
-//     tempArr = null;
+//     Object.assign(tempObj, { 'ind1': input[1], 'ind2': input[2] });
+//     current = tempObj;
+//     Object.assign(algoObj, { 'two': current });
+//     tempObj = null;
 //     current = null;
 //     counter = counter + 2;
-//     console.log(algoObj);
 //   }
 
 //   if (counter === 3) {
-//     let tempArr = [];
+//     let tempObj = {};
 
-//     tempArr.push(input[3], input[4], input[5]);
-//     current = tempArr;
-//     Object.assign(algoObj, { 'level 3': current });
-//     tempArr = null;
+//     Object.assign(tempObj, { 'ind1': input[3], 'ind2': input[4], 'ind3': input[5] });
+//     current = tempObj;
+//     Object.assign(algoObj, { 'three': current });
+//     tempObj = null;
 //     current = null;
 //     counter = counter + 3;
-//     console.log(algoObj);
 //   }
 
 //   if (counter === 6) {
-//     let tempArr = [];
+//     let tempObj = {};
 
-//     tempArr.push(input[6], input[7], input[8], input[9]);
-//     current = tempArr;
-//     Object.assign(algoObj, { 'level 4': current });
-//     tempArr = null;
+//     Object.assign(tempObj, {
+//       'ind1': input[6],
+//       'ind2': input[7],
+//       'ind3': input[8],
+//       'ind4': input[9]
+//     });
+//     current = tempObj;
+
+//     Object.assign(algoObj, { 'four': current });
+//     tempObj = null;
 //     current = null;
 //     counter = counter + 4;
-//     console.log(algoObj);
 //   }
 
 //   if (counter === 10) {
-//     let tempArr = [];
+//     let tempObj = {};
 
-//     tempArr.push(input[10], input[11], input[12], input[13], input[14]);
-//     current = tempArr;
-//     Object.assign(algoObj, { 'level 5': current });
-//     tempArr = null;
+//     Object.assign(tempObj, {
+//       'ind1': input[10],
+//       'ind2': input[11],
+//       'ind3': input[12],
+//       'ind4': input[13],
+//       'ind5': input[14]
+//     });
+//     current = tempObj;
+
+//     Object.assign(algoObj, { 'five': current });
+//     tempObj = null;
 //     current = null;
 //     counter = counter + 5;
-//     console.log(algoObj);
 //   }
 // }
 
 
-// console.timeEnd('time');
+// const navigate = (algoObj, Target) => {
+//   const start = algoObj.one;
+//   let top = null;
+//   for (const [key, value] of Object.entries(algoObj.two)) {
+
+//   }
+//   for (const [key, value] of Object.entries(algoObj.three)) {
+//     let res = value * top;
+//     console.log(res);
+//   }
+//   for (const [key, value] of Object.entries(algoObj.four)) {
+//     let res = value * top;
+//     console.log(res);
+//   }
+//   for (const [key, value] of Object.entries(algoObj.five)) {
+//     let res = value * top;
+//     console.log(res);
+//   }
+// };
+// navigate(algoObj);
+console.timeEnd('time');
+
+
